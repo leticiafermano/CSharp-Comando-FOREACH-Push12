eefe66845faa3384b96b3cb49520bba78a179218
Lethícia Santos <leticia-santos75@hotmail.com>
2022-02-17 20:15:04 -0300

    Adicionar arquivos de projeto.

---
+++ CSharp-Comando-FOREACH-Push12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Comando_FOREACH_Push12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
